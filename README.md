57c3a541c228ca9d06a7d53e3ae1b7a14c5293cd
VedhaSankar <vedhasankar26@gmail.com>
2023-06-13 14:52:39 +0530

    Updates for 14:52:39   13/06/23

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Use prometheus and k8s to monitor the metrics in K8s.
 
 ```
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install mongodb-exporter prometheus-community/prometheus-mongodb-exporter
 ```
 
 prom-aggregation-gateway
+
+https://github.com/prometheus/client_python
+https://www.youtube.com/watch?v=HzEiRwJP6ag
--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
+from prometheus_client import start_http_server, Summary
+import random
+import time
+
 load_dotenv(override=True)
 
 MONGO_URI = os.environ.get('MONGO_URI')
@@ -33,5 +37,24 @@ def start():
     return render_template('index.html')
 
 
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+
 if __name__== "__main__":
+
     app.run(host="0.0.0.0", debug = True, port = 5003)
+
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(random.random())
+        
